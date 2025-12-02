59ec72a8e62ef942d3283e3e3dd42f381f107ed1
gioroberta <102972472+gioroberta@users.noreply.github.com>
2025-12-02 15:41:20 -0300

    Exercícios laços de repetição 02/12

--- bloco1/listadeexercicios/lacosderepeticao.js
+++ bloco1/listadeexercicios/lacosderepeticao.js
@@ -1,6 +1,6 @@
 const prompt = require("readline-sync");
 
-// 1) DO Escreva um algoritmo, que leia 2 números inteiros via teclado, onde o primeiro número deve ser menor 
+// 1) FOR Escreva um algoritmo, que leia 2 números inteiros via teclado, onde o primeiro número deve ser menor 
 // do que o segundo número. Caso contrário, deve ser exibida uma mensagem na tela 
 // informando que o intervalo é inválido e sair do programa. 
 // No intervalo informado, mostre na tela todos os números que são múltiplos de 3 e 5. 
